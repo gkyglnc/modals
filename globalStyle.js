1a9bf33c46588937ab1857f4a78557b287ddae58
Gökay <144594856+gkyglnc@users.noreply.github.com>
2023-12-26 10:46:11 +0300

    Add files via upload

---
+++ globalStyle.js
@@ -0,0 +1,36 @@
+export const btnHoverStyle = {
+    cursor:"pointer",
+    "&:hover":{color:"red"}
+};
+export const modalStyle = {
+    position:"absolute",
+    top:"50%",
+    left:"50%",
+    transform:"translate(-50%, -50%)",
+    width:"900",
+    bgcolor:"background.paper",
+    border:"2px solid #000",
+    boxShadow:24,
+    p:6,
+    // overflow:"scroll",
+}
+export const flex ={
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    marginTop:"1rem",
+}
+export const flexw = {
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    gap:"1rem",
+    width:850,
+    marginTop:"1rem",
+}
+export const halfWidth = {
+    width:"48%",
+    display:"block",
+    marginBottom:"2rem",
+    gap:"1rem",
+}
